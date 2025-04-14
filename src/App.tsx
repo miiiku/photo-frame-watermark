b72ee6a40515089259bfd9c953f12d0f71671fcb
guanquanhongdesu <guanquanhongdesu@gmail.com>
2025-04-14 11:54:09 +0000

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <>
+      <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+      <button class="btn btn-neutral">Neutral</button>
+    </>
+  );
+};
+
+export default App;
